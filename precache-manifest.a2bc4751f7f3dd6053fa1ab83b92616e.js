1bfa9d63caa374773aad91d48f83d38873715e24
Chepkyi Mykola <chepkiyn@gmail.com>
2019-10-31 17:16:51 +0200

    Updates

---
+++ precache-manifest.a2bc4751f7f3dd6053fa1ab83b92616e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d679cfe3c76b7a7b77cb4fffc33df367",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "063170462d49e156801e",
+    "url": "/react_static-list-of-posts/static/css/main.970a0447.chunk.css"
+  },
+  {
+    "revision": "37a5fbf118a7aac249d7",
+    "url": "/react_static-list-of-posts/static/js/2.79440cf4.chunk.js"
+  },
+  {
+    "revision": "063170462d49e156801e",
+    "url": "/react_static-list-of-posts/static/js/main.5bc7b5be.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
